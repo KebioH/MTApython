3aa77de446a9750493fc9864d4830d665c766d68
KebioH <78331057+KebioH@users.noreply.github.com>
2021-02-01 10:45:43 +0800

    Add files via upload

---
+++ day 1-1-2.test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 10:03:11 2021
+
+@author: MTAEXAM
+"""
+
+height = float(input("上底:"))
+wide = float(input("下底:"))
+high = float(input("高:"))
+area = (height+wide)*high/2
+print("長方形面積:"+str(area))
