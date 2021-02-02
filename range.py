b67ce8c76680426794e3c1c3122f2623d6926940
KebioH <78331057+KebioH@users.noreply.github.com>
2021-02-02 09:18:36 +0800

    Add files via upload

---
+++ range.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 15:26:45 2021
+
+@author: MTAEXAM
+"""
+
+n = int(input(""))
+ for n in range(1,n + 1):
+        total *= n
+        print(total)
---
+++ untitled1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 15:13:23 2021
+
+@author: MTAEXAM
+"""
+
+
+n=1
+total=0
+while n<=10:
+    total+=n
+    n+=1
+print(total)
