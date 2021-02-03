69abbc0f7fb0299aa4a063c163e76f776f88122a
KebioH <78331057+KebioH@users.noreply.github.com>
2021-02-03 16:12:19 +0800

    Add files via upload

--- untitled1.py
+++ untitled1.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb  1 15:13:23 2021
+Created on Wed Feb  3 15:01:09 2021
 
 @author: MTAEXAM
 """
-
-
-n=1
-total=0
-while n<=10:
-    total+=n
-    n+=1
-print(total)
+f = open("9999.txt","r")
+for line in f:
+    print(line)
+f.close()
+with open("9999.txt","r") as f:
+    for line in f:
+        print(line)
